43a3c338edb995deeaededad939af46fe38f58cd
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2021-06-15 17:32:52 +0200

    affichage fonctionnel

--- main.py
+++ main.py
@@ -31,14 +31,48 @@ Case: {
 }
 
     path de mon dossier : \Drive\01_UTC\GI02\ProjetIA02
-    commande cmd : .\serveur\win64\crocomine-lite-beta3.exe :8000 .\serveur\grilles\
+        commande cmd : .\serveur\win64\crocomine-lite-beta3.exe :8000 .\serveur\grilles\
 '''
 from client.crocomine_client import CrocomineClient
 from pprint import pprint
+from typing import List, Tuple
+import subprocess
+from itertools import combinations
 
 
 def print_hi(name):
     print(f'Hi, {name}')
+
+
+# FONCTION IMPORTE DEPUIS LE TP SUDOKU
+def write_dimacs_file(dimacs: str, filename: str):
+    with open(filename, "w", newline="") as cnf:
+        cnf.write(dimacs)
+
+
+def exec_gophersat(
+        filename: str, cmd: str = "gophersat-1.1.6.exe", encoding: str = "utf8") -> Tuple[bool, List[int]]:
+    result = subprocess.run([cmd, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True,
+                            encoding=encoding)
+    string = str(result.stdout)
+    lines = string.splitlines()
+
+    if lines[1] != "s SATISFIABLE":
+        return False, []
+
+    model = lines[2][2:].split(" ")
+
+    return True, [int(x) for x in model]
+
+
+def clauses_to_dimacs(clauses, nb_vars):
+    res: str = ""
+    res += "p cnf " + str(nb_vars) + " " + str(len(clauses)) + "\n"
+    for clause in clauses:
+        for literal in clause:
+            res += str(literal) + " "
+        res += "0\n"
+    return res
 
 
 def processingInfos(infos, mat):
@@ -49,20 +83,30 @@ def processingInfos(infos, mat):
         j = info["pos"][1]
         mat[i][j]["isFieldKnown"] = True
         mat[i][j]["fieldType"] = info["field"]
-        if info["prox_count"]:
+        if "prox_count" in info:
             mat[i][j]["hasBeenCleared"] = True
             mat[i][j]["content"] = "safe"
             mat[i][j]["proxCount"] = info["prox_count"]
     return
 
+
 def affichageMat(gridInfos, matInfo):
     print("Affichage de l'état des choses")
-    for i in range(gridInfos["n"]):
-        for j in range(gridInfos["m"]):
-            if(matInfo[i][j]["hasBeenCleared"]):
-                if(matInfo[i][j]["content"] == "safe"):
-                    print("O", sep='')
-
+    for i in range(gridInfos["m"]):
+        for j in range(gridInfos["n"]):
+            if (matInfo[i][j]["hasBeenCleared"]):
+                if (matInfo[i][j]["content"] == "safe"):
+                    print("O", end=' ')
+                else:
+                    print(matInfo[i][j]["content"], end=' ')
+            elif matInfo[i][j]["isFieldKnown"]:
+                if matInfo[i][j]["fieldType"] == "sea":
+                    print("~", end=' ')
+                else:
+                    print("x", end=' ')
+            else:
+                print("?", end=' ')
+        print(" ")
     return
 
 
@@ -80,8 +124,6 @@ def a_game():
     matInfo = [[{"isFieldKnown": False, "fieldType": "unknown", "hasBeenCleared": False, "content": "unknown",
                  "proxCount": (-1, -1, -1), "clearedProx": (0, 0, 0)} for j in range(gridInfos["n"])] for i in
                range(gridInfos["m"])]
-
-
 
     # On fait un premier discover sur la case de départ, et on traite ce discover
     status, msg, infos = croco.discover(gridInfos["start"][0], gridInfos["start"][1])
